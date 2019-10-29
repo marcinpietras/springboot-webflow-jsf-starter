8ed554913fc9b597d3d6a6e1b55d14fa3d6d0944
Marcin Pietras <mpietras@lendingclub.com>
2019-10-29 10:27:56 -0700

    Initial commit

---
+++ src/main/java/com/housegap/springbootwebflowjsfstarter/SpringbootWebflowJsfStarterApplication.java
@@ -0,0 +1,13 @@
+package com.housegap.springbootwebflowjsfstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebflowJsfStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebflowJsfStarterApplication.class, args);
+	}
+
+}
